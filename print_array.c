2f0b4b2c52dd4b6a13485045c4af8ad760bb2241
OlivetEkeh <ekehsons@gmail.com>
2024-01-19 15:53:02 +0100

    1st tast

--- print_array.c
+++ print_array.c
@@ -1,30 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "sort.h"
 
 /**
- * print_array - Prints an array of integers
+ * print_array - This prints an array of integers
  *
- * @array: The array to be printed
- * @size: Number of elements in @array
+ * @array: This is the array to be printed
+ * @size: this is the number of elements in @array
  */
 void print_array(const int *array, size_t size)
 {
-	size_t i;
+        size_t i;
 
-	i = 0;
-	while (array && i < size)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", array[i]);
-		++i;
-	}
-	printf("\n");
+        i = 0;
+        while (array && i < size)
+        {
+                if (i > 0)
+                        printf(", ");
+                printf("%d", array[i]);
+                ++i;
+        }
+        printf("\n");
 }
 
-
-#include <stdio.h>
-#include "sort.h"
 
 /**
  * print_list - Prints a list of integers
