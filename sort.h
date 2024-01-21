a428ceec00d6149cf5d578b03262cde9f1de24ef
OlivetEkeh <ekehsons@gmail.com>
2024-01-21 10:44:09 +0100

    100task

--- sort.h
+++ sort.h
@@ -30,6 +30,6 @@ void quick_sort(int *array, size_t size);
 void sort_array(int *array, size_t low, size_t high, size_t size);
 int partition(int array[], size_t low, size_t high, size_t size);
 void swap(int *a, int *b);
-
+void shell_sort(int *array, size_t size);
 
 #endif
