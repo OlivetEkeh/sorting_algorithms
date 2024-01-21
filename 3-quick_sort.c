85a1bf955371dfb678b9b3e1150da129e303b9ff
OlivetEkeh <ekehsons@gmail.com>
2024-01-21 10:10:57 +0100

    task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,14 +1,14 @@
 #include "sort.h"
 
 /**
- * quick_sort - sorts the array in ascending order
- * using Lomuto's quick sort algorithm
- * @array: the array to be sorted
- * @size: the size of the array
+ * quick_sort - This sorts the array in ascending order
+ * using the Lomuto's quick sort algorithm
+ * @array: this is the array to be sorted
+ * @size: this the size of the array
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array && size > 1)
+	if (array != NULL && size > 1)
 		sort_array(array, 0, size - 1, size);
 }
 
